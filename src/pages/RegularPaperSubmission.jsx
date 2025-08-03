6e25381499b9420a626fd2e82aa955d7bd526ee7
vyyvek <beingviveksharma99@gmail.com>
2025-08-03 10:30:56 +0530

    hello

--- src/pages/RegularPaperSubmission.jsx
+++ src/pages/RegularPaperSubmission.jsx
@@ -63,7 +63,7 @@ const RegularPaperSubmission = () => {
           </ul>
           <p className="paper-submission-note">
   {/* <span className="note-label"> </span> */}
-  CMT Ackhnowledgment :  The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.
+  CMT Acknowledgement :  The Microsoft CMT service was used for managing the peer-reviewing process for this conference. This service was provided for free by Microsoft and they bore all expenses, including costs for Azure cloud services as well as for software development and support.
 </p>
 
         </div>
